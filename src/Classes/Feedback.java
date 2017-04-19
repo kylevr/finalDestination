78896051862170060364293ebca4df6492e79241
samdirkx1 <271146@student.fontys.nl>
2017-04-19 15:10:44 +0200

    classes aangepast ivm feedback
    Feedback
    User
    Connection

--- src/Classes/Feedback.java
+++ src/Classes/Feedback.java
@@ -8,16 +8,64 @@ public class Feedback {
     private Date timeCreated;
     private int rating;
     private String description;
-    private List<User> userFrom;
-    private List<User> userTo;
+    private String userFrom_Username;
+    private String userTo_Username;
+
+    public Date getTimeCreated() {
+        return timeCreated;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUserFrom_Username() {
+        return userFrom_Username;
+    }
+
+    public String getUserTo_Username() {
+        return userTo_Username;
+    }
     
-    
-    public Feedback(User from, User to, int rating, String description)
+    /**
+     * constructor used to created a new feedback object that never existed before 
+     * @param userFrom_Username
+     * @param userTo_Username
+     * @param rating
+     * @param description 
+     */
+    public Feedback(String userFrom_Username, String userTo_Username, int rating, String description)
     {
-        this.userFrom.add(from);
-        this.userTo.add(to);
+        timeCreated = new Date();
+        this.userFrom_Username = userFrom_Username;
+        this.userTo_Username = userTo_Username;
         this.rating = rating;
         this.description = description;
     }
 
+    /**
+     * constructor used to created a new feedback object that existed before in the database.
+     * @param timeCreated
+     * @param userFrom_Username
+     * @param userTo_Username
+     * @param rating
+     * @param description 
+     */
+    public Feedback(Date timeCreated, String userFrom_Username, String userTo_Username, int rating, String description)
+    {
+        this.timeCreated = timeCreated;
+        this.userFrom_Username = userFrom_Username;
+        this.userTo_Username = userTo_Username;
+        this.rating = rating;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "timeCreated:" + timeCreated + ", rating:" + rating + ", description:" + description + ", userFrom_Username:" + userFrom_Username + ", userTo_Username:" + userTo_Username + '}';
+    }
 }
--- src/Classes/User.java
+++ src/Classes/User.java
@@ -27,6 +27,7 @@ public class User {
     private List<Auction> placedAuctions;
     private List<Queue_Purchase> placedOrders;
     private List<Transaction> transactions;
+    private List<Feedback> feedbacklist;
 
     /**
      * constructor for a user that gets initialized by supplying his name and password.
@@ -51,6 +52,7 @@ public class User {
         this.placedAuctions = myUser.placedAuctions;
         this.placedOrders = myUser.placedOrders;
         this.transactions = myUser.transactions;
+        this.feedbacklist = myUser.feedbacklist;
     }
     
     /**
@@ -84,7 +86,28 @@ public class User {
         this.placedAuctions = new ArrayList<Auction>();
         this.placedOrders = new ArrayList<Queue_Purchase>();
         this.transactions = new ArrayList<Transaction>();
-    }   
+        this.feedbacklist = new ArrayList<Feedback>();
+    }
+    
+    /**
+     * constructor for a user with everything manually inputted.
+     * userid isn't given here because that's a database thing
+     */
+    public User(int BSN, String username, String password, String alias, String email, boolean verified, double saldo, String imageURL) {
+        this.BSN = BSN;
+        this.username = username;
+        this.password = password;
+        this.alias = alias;
+        this.email = email;
+        this.verified = verified;
+        this.saldo = saldo;
+        this.imageURL = imageURL;
+        this.bids = new ArrayList<Bid>();
+        this.placedAuctions = new ArrayList<Auction>();
+        this.placedOrders = new ArrayList<Queue_Purchase>();
+        this.transactions = new ArrayList<Transaction>();
+        this.feedbacklist = new ArrayList<Feedback>();
+    }
     
     public int getUserID() {
         return userID;
@@ -212,5 +235,79 @@ public class User {
     
     public String getImageURL() {
         return imageURL;
+    }
+    
+    public void addFeedback(Feedback feedback)
+    {
+        if (!this.feedbacklist.contains(feedback))
+        {
+            this.feedbacklist.add(feedback);
+        }
+    }
+
+    public void removeFeedback(Feedback feedback)
+    {
+        this.feedbacklist.remove(feedback);
+    }
+    
+    
+    /**
+     * Receives the list with feedback addressed to user object
+     *
+     * @return a list with feedback objects
+     */
+    public List<Feedback> getFeedbackToMe() {
+        List<Feedback> feedbackToMeList = new ArrayList<Feedback>();
+        for (Feedback f : this.feedbacklist)
+        {
+           if (f.getUserTo_Username().equals(this.username))
+            {
+                feedbackToMeList.add(f);
+            }
+        }
+        
+        return feedbackToMeList;
+    }
+    
+    /**
+     * Receives the list with feedback sended by user object
+     *
+     * @return a list with feedback objects
+     */
+    public List<Feedback> getFeedbackFromMe() {
+        List<Feedback> feedbackFromMeList = new ArrayList<Feedback>();
+        for (Feedback f : this.feedbacklist)
+        {
+            if (f.getUserFrom_Username().equals(this.username))
+            {
+                feedbackFromMeList.add(f);
+            }
+        }
+        
+        return feedbackFromMeList;
+    }
+    
+    /**
+     * Receives the list with all feedback related to user object
+     *
+     * @return a list with feedback objects
+     */
+    public List<Feedback> getFeedbacklist() {
+        return this.feedbacklist;
+    }
+    
+    public void updateFeedbacklist()
+    {
+        Connection conn = new Connection();
+        conn.getConnection();
+        this.feedbacklist.clear();
+        for (Feedback f : conn.getFeedbackToSeller(this.username))
+        {
+            this.addFeedback(f);
+        }
+        for (Feedback f : conn.getFeedbackFromBuyer(this.username))
+        {
+            this.addFeedback(f);
+        }
     }
 }
