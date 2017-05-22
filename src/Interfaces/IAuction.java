0876baf5dcfba3b34d7a972db27ec0c9aaeca52b
Pieter Beukelman <pieter@beukelman.eu>
2017-05-22 09:56:16 +0200

    IAuction

--- src/Interfaces/IAuction.java
+++ src/Interfaces/IAuction.java
@@ -5,10 +5,53 @@
  */
 package Interfaces;
 
+import Classes.Auctions.Auction;
+import Classes.Bid;
+import Classes.Feedback;
+import Classes.User;
+import java.util.List;
+
 /**
  *
  * @author piete
  */
 public interface IAuction {
+
+    /**
+     *returns all auctions 
+     * @return list of auctions
+     */
+    List<Auction> getAuctions();
+
+    /**
+     *returns list of Bids with auction id
+     * @param auctionId id of corresponding auction
+     * @return list of bids
+     */
+    List<Bid> getBids(int auctionId);
     
+    /**
+     *returns the id of the seller of the auction
+     * @param auctionId id of corresponding auction
+     * @return seller
+     */
+    User getSeller(int auctionId);
+    
+    /**
+     *adds a auction tot the database
+     * @param auction auction to be added
+     */
+    void addAuction(Auction auction);
+    
+    /**
+     *adds feedback to a seller
+     * @param feedback feedback to be added
+     */
+    void addFeedback(Feedback feedback);
+    
+    /**
+     * sends a mail to the Seller
+     * @param content content to be send to seller
+     */
+    void sendMail(String content);
 }
